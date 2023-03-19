eff565079419e7d29b44cb3dce1364a0d51ed55f
Scott Keck-Warren <warren5236@gmail.com>
2023-03-19 12:53:50 -0400

    Added tests

---
+++ src/Name/LastName.php
@@ -0,0 +1,17 @@
+<?php
+namespace ScottsValueObjects\Name;
+
+class LastName
+{
+    protected string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function length(): int
+    {
+        return strlen($this->value);
+    }
+}

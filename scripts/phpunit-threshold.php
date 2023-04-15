84779bec883d73a315fa630aadd25f16dcff12e3
Scott Keck-Warren <warren5236@gmail.com>
2023-04-14 20:39:26 -0400

    Cleanup

--- scripts/phpunit-threshold.php
+++ scripts/phpunit-threshold.php
@@ -11,11 +11,11 @@ $threshold = (double) $argv[2];
 $coverage = simplexml_load_file($file);
 $ratio = (double) ($coverage->project->metrics["coveredstatements"] / $coverage->project->metrics["statements"] * 100);
 
-echo "Line coverage: $ratio%";
-echo "Threshold: $threshold%";
+echo "Line coverage: $ratio%", PHP_EOL;
+echo "Threshold: $threshold%", PHP_EOL;
 
 if ($ratio < $threshold) {
-    echo "FAILED!";
+    echo "FAILED!", PHP_EOL;
     exit(-1);
 }
-echo "SUCCESS!";
+echo "SUCCESS!", PHP_EOL;
